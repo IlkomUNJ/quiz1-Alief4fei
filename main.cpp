ce8041c2f81cd1bcee8aa7fb4dd94ea072b77e52
Alief4fei <alief.fadillah202@gmail.com>
2025-10-01 12:42:39 +0700

    1313624046

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 #include "buyer.h"
 
 enum PrimaryPrompt{LOGIN, REGISTER, EXIT};
+enum regprompt{reg_buyer, reg_exit};
+enum accountmenu{upgrade_account, exit_to_main_menu, exit_program};
+
+BankCustomer customer1(1, "Alice", 1000.0);
+Buyer buyer1(1, customer1.getName(), customer1);
 
 using namespace std;
 
@@ -18,8 +23,65 @@ int main() {
         cin >> choice;
         prompt = static_cast<PrimaryPrompt>(choice - 1);
         switch (prompt) {
-            case LOGIN:
+            case LOGIN: {
                 cout << "Login selected." << endl;
+                string inputName;
+                int inputId;
+
+                cout << "Enter your ID: ";
+                cin >> inputId;
+                cout << "Enter your Name: ";
+                cin.ignore(); 
+                getline(cin, inputName);
+
+                if (inputId == customer1.getId() && inputName == customer1.getName())
+                {
+                    cout << "Login successful. Welcome, " << inputName << "!" << endl;
+
+                    accountmenu menuPromt = exit_to_main_menu;
+                    while (menuPromt != exit_program)
+                    {
+                        cout << "Select an option: " << endl;
+                        cout << "1. Upgrade Account to Seller" << endl;
+                        cout << "2. Exit to Main Menu" << endl;
+                        cout << "3. Exit Program" << endl;
+                        int menu_choice;
+                        cin >> menu_choice;
+                        menuPromt = static_cast<accountmenu>(menu_choice - 1);
+                        switch (menuPromt) {
+                            case upgrade_account:
+                                cout << "Upgrade Account to Seller selected." << endl;
+                                
+
+
+                                break;
+                            case exit_to_main_menu:
+                                cout << "Exiting to main menu." << endl;
+                                break;
+                            case exit_program:
+                                cout << "Exiting program." << endl;
+                                break;
+                            default:
+                                cout << "Invalid option." << endl;
+                                break;
+                        }
+                        cout << endl;
+                    }
+                    
+
+                    cout << "account menu:" << endl;
+                    
+                    
+                }
+                else
+                {
+                    cout << "Login failed. Invalid ID or Name." << endl;
+                    prompt = LOGIN;
+                    break;
+                }
+            }
+                
+                
                 /* if Login is selected, based on authority then provide options:
                 assume user is logged in as Buyer for now
                 1. Chek Account Status (will display if user is Buyer or Seller or both and linked banking account status)
@@ -71,8 +133,79 @@ int main() {
                 10. Exit Program
                 **/
                 break;
-            case REGISTER:
+            case REGISTER: {
                 cout << "Register selected." << endl;
+                regprompt reg_prompt = reg_buyer;
+                while (reg_prompt != reg_exit) {
+                    cout << "Select an option: " << endl;
+                    cout << "1. Buyer" << endl;
+                    cout << "2. Exit" << endl;
+                    int reg_choice;
+                    cin >> reg_choice;
+                    reg_prompt = static_cast<regprompt>(reg_choice - 1);
+                    switch (reg_prompt) {
+                        case reg_buyer:
+                            {
+                                cout << "Buyer selected." << endl;
+                                string name, address, phone, email;
+                                cout << "Enter your name: ";
+                                cin.ignore();
+                                getline(cin, name);
+                                cout << "Enter your home address: ";
+                                getline(cin, address);
+                                cout << "Enter your phone number: ";
+                                getline(cin, phone);
+                                cout << "Enter your email: ";
+                                getline(cin, email);
+
+                                BankCustomer customer1(1, name, 0);
+                                Buyer buyer1(1, name, customer1);
+                                cout << "Buyer account created for " << buyer1.getName() << " with ID " << buyer1.getId() << endl;
+
+                                cout << "Do you want to create a Seller account? (y/n): ";
+                                char seller_choice;
+                                cin >> seller_choice;
+                                if (seller_choice == 'y' || seller_choice == 'Y') {
+                                    string store_name, store_address, store_phone, store_email;
+                                    cout << "Enter your store name: ";
+                                    cin.ignore();
+                                    getline(cin, store_name);
+                                    cout << "Enter your store address: ";
+                                    getline(cin, store_address);
+                                    cout << "Enter your store phone number: ";
+                                    getline(cin, store_phone);
+                                    cout << "Enter your store email: ";
+                                    getline(cin, store_email);
+                                    
+
+
+                                    cout << "Seller account created for store " << store_name << endl;
+
+                                    reg_prompt = reg_exit;
+                                    break;
+                                } 
+
+                                else {
+                                    cout << "No Seller account created." << endl;
+                                    reg_prompt = reg_exit;
+                                    break;
+                                }
+
+                            }
+                            break;
+                        
+                        case reg_exit:
+                            cout << "Exiting." << endl;
+                            break;
+                        default:
+                            cout << "Invalid option." << endl;
+                            break;
+                    }
+                    cout << endl;
+                }
+    
+                break;
+            }
                 /* if register is selected then went throuhh registration process:
                 1. Create a new Buyer Account
                 Must provides: Name, Home Address, Phone number, Email
